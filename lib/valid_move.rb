cd3d86b1ce2f04666c7163e2ea3cdcf978b7cbbc
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-23 15:24:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
-def valid_move?
-  if board[position] == (0...8)
+def valid_move?(board,position)
+    if board[position] == (0...8)
     true
   elsif position_taken? == false
     true
